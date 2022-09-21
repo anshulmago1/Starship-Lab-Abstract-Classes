64dd130799b73bdbe36eeba53bce920a54e9a164
Anshul Mago <magoanshul@gmail.com>
2022-09-21 13:11:36 -0500

    Revised Starfighter starter code

--- StarFighter.java
+++ StarFighter.java
@@ -1,27 +1,25 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-
 import javax.swing.JFrame;
 import java.awt.Component;
+import java.awt.Graphics;
 
 public class StarFighter extends JFrame
 {
-	private static final int WIDTH = 800;
-	private static final int HEIGHT = 600;
+	public static int WIDTH = 800;
+	public static int HEIGHT = 600;
 
 	public StarFighter()
 	{
 		super("STARFIGHTER");
-		setSize(WIDTH,HEIGHT);
+
+		setSize(WIDTH, HEIGHT);
 
 		OuterSpace theGame = new OuterSpace();
 		((Component)theGame).setFocusable(true);
 
-		getContentPane().add(theGame);
-
+                getContentPane().add( theGame );
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-	}
+	} 
 
 	public static void main( String args[] )
 	{
