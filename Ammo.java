adc0e76c8211c182a7eeaac7ad8260216bec3685
Timothy Liu <timothy.l.6uy@gmail.com>
2022-09-24 23:26:57 -0500

    Completed Ammo.java

--- Ammo.java
+++ Ammo.java
@@ -6,50 +6,65 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
+import java.net.URL;
 import javax.imageio.ImageIO;
 
 public class Ammo extends MovingThing
 {
 	private int speed;
+	private Image image;
 
 	public Ammo()
 	{
-		this(0,0,0);
+		this(0,0,10);
 	}
 
 	public Ammo(int x, int y)
 	{
-		//add code
+		this(x, y, 10);
 	}
 
 	public Ammo(int x, int y, int s)
 	{
-		//add code
+		super(x, y);
+		speed = s;
+		try
+		{
+			URL url = getClass().getResource("ammo.jpg");
+			image = ImageIO.read(url);
+		}
+		catch(Exception e)
+		{
+			//feel free to do something here
+			System.out.println("Alien.java: incorrect image path");
+		}
 	}
 
 	public void setSpeed(int s)
 	{
-	   //add code
+	   speed = s;
 	}
 
 	public int getSpeed()
 	{
-	   return 0;
+	   return speed;
 	}
 
 	public void draw( Graphics window )
 	{
-		//add code to draw the ammo
+		window.drawImage(image,getX(),getY(),getWidth(),getHeight(), null);
 	}
 	
 	
 	public void move( String direction )
 	{
 		//add code to draw the ammo
+		if (direction.equals("SPACE"))
+			setY(getY()-speed);
 	}
 
 	public String toString()
 	{
-		return "";
+		return super.toString() + "Speed: " + getSpeed();
 	}
 }
